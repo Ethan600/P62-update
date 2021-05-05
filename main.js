568093116699a41dd7ca66b49b6b447685cd6039
Ethan600 <77286763+Ethan600@users.noreply.github.com>
2021-05-05 19:57:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function divalert(){
+    window.alert("This is Captain America, Black Panther, The Incredible Hulk and Iron Man");
+}
+function divalert(){
+    window.alert("This is Thanos, Thor, Gamora and Captain Marvel");
+}
